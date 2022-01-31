6949fdcbcf43d01b67f8192407c47cc9e5ad0334
galaddirie <70884733+galaddirie@users.noreply.github.com>
2022-01-31 04:33:36 -0500

    Disabled Cache Logs

--- jg_diff/settings.py
+++ jg_diff/settings.py
@@ -92,7 +92,7 @@ CASSIOPEIA_PIPELINE = {
         "MAX_ENTRIES": 6000,
         "CULL_FRECUENCY": 2,
         "SAFE_CHECK": True,
-        "LOGS_ENABLED": True,
+        "LOGS_ENABLED": False,
     },
     "DjangoCache": [
         {
