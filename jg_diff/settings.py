c77fa745e10199915f235f4aa921fdb45dd884c5
galaddirie <70884733+galaddirie@users.noreply.github.com>
2022-02-02 14:27:25 -0500

    Removed Api Key Default

--- jg_diff/settings.py
+++ jg_diff/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", '12345678910')
 RIOT_API_KEY = os.environ.get(
-    "RIOT_API_KEY", 'RGAPI-b94771b6-c67d-4a91-8801-23d19baf64fd')
+    "RIOT_API_KEY", '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG_VALUE", 'True') == 'True'
